97e6685cca37944e02dc12940b3cc5315a137402
syafii <122841714+Piol0@users.noreply.github.com>
2023-09-02 16:11:21 +0700

    Update bola.py

--- bola.py
+++ bola.py
@@ -1,7 +1,5 @@
 #Menghitung Volume Bola
 
-phi = 22/7
-
 jari = int(input("masukkan jari-jari "))
 
-print ("volume dari bola tersebut adalah",4/3*phi*(jari*jari*jari))
+print ("volume dari bola tersebut adalah",4/3*22/7*(jari*jari*jari))
