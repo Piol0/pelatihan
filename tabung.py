49d5667883232ce963040bc5a7000f141ea85eac
syafii <122841714+Piol0@users.noreply.github.com>
2023-09-02 16:10:54 +0700

    Update tabung.py

--- tabung.py
+++ tabung.py
@@ -1,9 +1,7 @@
 #Menghitung Volume Tabung
 
-phi = 22/7
-
 jari = int(input("masukkan jari-jari "))
 tinggi = int(input("masukkan tinggi "))
 
-print ("volume dari tabung tersebut adalah",phi*jari*jari*tinggi)
+print ("volume dari tabung tersebut adalah",22/7*jari*jari*tinggi)
 
